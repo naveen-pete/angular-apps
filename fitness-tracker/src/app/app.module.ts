77c4bf6b75e845fcb29a44332aef3712a2b9a615
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-03-20 15:27:45 +0530

    commit initial app into the repo

---
+++ fitness-tracker/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
