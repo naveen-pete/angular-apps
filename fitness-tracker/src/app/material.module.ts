d3a600c2329fb717b10c6701ff29d389e221ceaf
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-03-22 13:20:27 +0530

    add date picker component to the signup form

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from "@angular/material/icon";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
-const modules = [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule];
+const modules = [
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+];
 
 @NgModule({
   imports: [...modules],
