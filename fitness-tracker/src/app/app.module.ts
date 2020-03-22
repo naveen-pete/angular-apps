e5851a8ba49441b3944bd685c36d36e900c1fb47
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-03-22 13:37:36 +0530

    add terms & conditions checkbox, disable submit button when form controls are not valid, provide spacing between form controls

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
