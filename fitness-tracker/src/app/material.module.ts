ebcb922a11e3d3241dcc52b5af34bc63c9e27dd2
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-03-22 20:19:54 +0530

    add tabs for new and past exercises, add card for new exercise

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -10,6 +10,8 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatListModule } from "@angular/material/list";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   exports: [
@@ -22,7 +24,9 @@ import { MatListModule } from "@angular/material/list";
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
