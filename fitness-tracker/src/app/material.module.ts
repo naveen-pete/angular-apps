56581915af6070c6eaa63685d88fe4acb5d8ae40
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-03-22 12:26:35 +0530

    add components for login, signup, current training, new training, past trainings and home

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+
+const modules = [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule];
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [...modules],
+  exports: [...modules]
 })
 export class MaterialModule { }
