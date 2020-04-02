7d194feefe70c7b269edc6bd7916da1a1c2c3051
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-04-02 06:57:10 +0530

    commit changes to environment file

--- fitness-tracker/src/environments/environment.ts
+++ fitness-tracker/src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBi2Vlj-747C4YZAWiD7ZGEZjjcAPXKuvM",
-    authDomain: "angular-apps-25558.firebaseapp.com",
-    databaseURL: "https://angular-apps-25558.firebaseio.com",
-    projectId: "angular-apps-25558",
-    storageBucket: "angular-apps-25558.appspot.com",
-    messagingSenderId: "684129463828",
-    appId: "1:684129463828:web:c7bfa2fdbaa5011cd04101",
-    measurementId: "G-Q6N4FFE31D"
-  }
 };
 
 /*
