3a87632423fed577f9c5cf95a19cd44e81b05a78
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-04-03 12:28:45 +0530

    remove references to body-parser module

--- posts/posts-server/app.js
+++ posts/posts-server/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
