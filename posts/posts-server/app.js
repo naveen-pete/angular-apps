13ca20dd4a80afab037f80b6d828b79592d468fb
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-04-04 15:02:06 +0530

    commit minor changes to code

--- posts/posts-server/app.js
+++ posts/posts-server/app.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-const Post = require("./models/post");
+const Post = require('./models/post');
 
+dotenv.config();
 const app = express();
-require('dotenv').config();
 
 const connectToDb = async () => {
   try {
@@ -26,43 +27,43 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
   );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
   });
   post.save().then(createdPost => {
     res.status(201).json({
-      message: "Post added successfully",
+      message: 'Post added successfully',
       postId: createdPost._id
     });
   });
 });
 
-app.get("/api/posts", (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
   Post.find().then(documents => {
     res.status(200).json({
-      message: "Posts fetched successfully!",
+      message: 'Posts fetched successfully!',
       posts: documents
     });
   });
 });
 
-app.delete("/api/posts/:id", (req, res, next) => {
+app.delete('/api/posts/:id', (req, res, next) => {
   Post.deleteOne({ _id: req.params.id }).then(result => {
-    res.status(200).json({ message: "Post deleted!" });
+    res.status(200).json({ message: 'Post deleted!' });
   });
 });
 
